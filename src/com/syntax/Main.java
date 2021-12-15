72502c763b4a5ee4c4dbd57a3e6d3c79e46c3d4f
zleng12 <91295233+zleng12@users.noreply.github.com>
2021-12-14 21:21:48 -0500

    First commit

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,11 @@
+package com.syntax;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException{
+
+        for ( int i =0; i < 5; i++){
+            System.out.println("Batch 10 is great");
+            Thread.sleep(2000);
+        }
+    }
+}
